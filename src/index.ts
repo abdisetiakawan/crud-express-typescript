5604f03c944168cadb33190ac0b57c2210f3845b
abdisetiakawan <bedikadiryt@gmail.com>
2024-10-02 10:06:14 +0700

    Setup Husky

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import express, {
-  type Application,
-  type Request,
-  type Response,
-  type NextFunction
-} from 'express'
+import express, { type Application, type Request, type Response } from 'express'
 import 'dotenv/config'
 
 const app: Application = express()
 const port: number = Number(process.env.PORT) || 3000
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
