4ce2af6fb3d2b8a7ff246be5df54c5d526296e91
abdisetiakawan <bedikadiryt@gmail.com>
2024-10-03 07:55:59 +0700

    feat: Add validation and routing for barang with Joi
    - Added POST route with input validation for /barang using Joi
    - Defined barang validation schema with detailed error messages
    - Created BarangType interface for better type safety

--- src/routes/barang.route.ts
+++ src/routes/barang.route.ts
@@ -1,9 +1,27 @@
 import { type Request, type Response, Router } from 'express'
+import { inputBarangValidation } from '../validations/barang.validation'
 const barangRouter = Router()
 
 barangRouter.get('/barang', (req: Request, res: Response) => {
   res.status(200).json({
     message: 'Hello World'
+  })
+})
+
+barangRouter.post('/barang', (req: Request, res: Response) => {
+  const { error, value } = inputBarangValidation(req.body)
+  if (error) {
+    res.status(400).json({
+      error: error.details[0].message,
+      message: 'Input Data Gagal',
+      data: value
+    })
+  }
+
+  res.status(200).json({
+    error: null,
+    message: 'Input Data Berhasil',
+    data: value
   })
 })
 
